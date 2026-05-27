a4ace1e052ab56960ddf75d81a5cba77daf1e6b3
Vijay V <vijayvraj2000@gmail.com>
2026-05-27 16:54:18 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/To-Do-App-with-Authentication/",
   plugins: [react()],
   server: {
     port: 5173
-  }
+  },
+  base: "/To-Do-App-with-Authentication/"
 });
